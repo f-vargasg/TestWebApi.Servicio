274b1d0dbc03e78766a9f538a2df488f8fd41def
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-10-14 18:32:08 -0600

    Cambios realizados en computadora portatil

--- WinTestConsumeWebApi/FrmPrincTestWebApi.Designer.cs
+++ WinTestConsumeWebApi/FrmPrincTestWebApi.Designer.cs
@@ -29,13 +29,18 @@ namespace WinTestConsumeWebApi
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincTestWebApi));
             this.label1 = new System.Windows.Forms.Label();
             this.txtUrl = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.txtOutput = new System.Windows.Forms.TextBox();
+            this.tlsStrMenuPrinc = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.tlsStrButExit = new System.Windows.Forms.ToolStripButton();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.tlsStrMenuPrinc.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -65,6 +70,9 @@ namespace WinTestConsumeWebApi
             // 
             // panel2
             // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.Controls.Add(this.txtOutput);
             this.panel2.Location = new System.Drawing.Point(2, 142);
             this.panel2.Name = "panel2";
@@ -82,11 +90,44 @@ namespace WinTestConsumeWebApi
             this.txtOutput.Size = new System.Drawing.Size(895, 358);
             this.txtOutput.TabIndex = 2;
             // 
+            // tlsStrMenuPrinc
+            // 
+            this.tlsStrMenuPrinc.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.tlsStrMenuPrinc.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.tlsStrButExit});
+            this.tlsStrMenuPrinc.Location = new System.Drawing.Point(0, 0);
+            this.tlsStrMenuPrinc.Name = "tlsStrMenuPrinc";
+            this.tlsStrMenuPrinc.Size = new System.Drawing.Size(902, 31);
+            this.tlsStrMenuPrinc.TabIndex = 4;
+            this.tlsStrMenuPrinc.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 28);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // tlsStrButExit
+            // 
+            this.tlsStrButExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlsStrButExit.Image = ((System.Drawing.Image)(resources.GetObject("tlsStrButExit.Image")));
+            this.tlsStrButExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsStrButExit.Name = "tlsStrButExit";
+            this.tlsStrButExit.Size = new System.Drawing.Size(29, 28);
+            this.tlsStrButExit.Text = "toolStripButton2";
+            this.tlsStrButExit.ToolTipText = "Exit";
+            this.tlsStrButExit.Click += new System.EventHandler(this.tlsStrButExit_Click);
+            // 
             // FrmPrincTestWebApi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(902, 503);
+            this.Controls.Add(this.tlsStrMenuPrinc);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "FrmPrincTestWebApi";
@@ -95,7 +136,10 @@ namespace WinTestConsumeWebApi
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.tlsStrMenuPrinc.ResumeLayout(false);
+            this.tlsStrMenuPrinc.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -106,6 +150,9 @@ namespace WinTestConsumeWebApi
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.ToolStrip tlsStrMenuPrinc;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton tlsStrButExit;
     }
 }
 
--- WinTestConsumeWebApi/FrmPrincTestWebApi.cs
+++ WinTestConsumeWebApi/FrmPrincTestWebApi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -20,7 +21,13 @@ namespace WinTestConsumeWebApi
 
         private void InitMyComponents()
         {
+            this.Text = ConfigurationManager.AppSettings["captionForm"];
             this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void tlsStrButExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
         }
     }
 }
